0c10bd7cdaf54f8b0ffc5912026d57e2e5be9340
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-09-19 18:44:44 +0200

    Add store filter

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,3 +3,10 @@ import type {ToggleOptions} from "@components/PageHeader/Filter/Filter.tsx";
 
 export const TIDLIGERE_HASH = "#tidligere"
 export const $showTidligere: PreinitializedWritableAtom<boolean>   = atom(window.location.hash === TIDLIGERE_HASH);
+
+export const $filterSearch: PreinitializedWritableAtom<string>  = atom("");
+export const $filterVarselType: PreinitializedWritableAtom<ToggleOptions>  = atom("alle");
+
+export const setFilterSearch = (search: string) => {
+    $filterSearch.set(search)
+}
