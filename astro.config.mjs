59a604d27abdb0bb2b52488189e653b462c44868
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-10-07 08:12:43 +0200

    Updated base-path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/minside/varsler",
+  base: "/minside/varsler-ny",
   build: {
     assetsPrefix: "https://cdn.nav.no/min-side/tms-varsler-frontend"
   },
