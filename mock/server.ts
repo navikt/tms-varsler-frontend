ae90ad1a635dffd86dc15fd49783438ce98c1653
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-10-31 15:51:56 +0100

    Fix mock

--- mock/server.ts
+++ mock/server.ts
@@ -1,15 +1,16 @@
 import {Hono} from "hono";
 import {serve} from "@hono/node-server";
 import {cors} from "hono/cors";
-import * as aktiveVarsler from './data/aktive.json';
+import * as varsler from './data/varsler.json';
 
 const api = new Hono();
+
 
 api.use("/*", cors({
     origin: "http://localhost:4321",
     credentials: true,
 }));
 
-api.get('/tms-varsel-api/alle', (c) => c.json(aktiveVarsler));
+api.get('/tms-varsel-api/alle', (c) => c.json(varsler));
 
 serve(api);
