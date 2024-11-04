2c9168caebf00213274b914f5807e1dbf313b70a
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-11-04 07:21:51 +0100

    Vise error uavhening av visning

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,14 +1,10 @@
 import {Alert, BodyLong} from "@navikt/ds-react";
 import {text} from "@language/text.ts";
 import {DOCUMENT_LOCALE} from "@language/language.ts";
-import {useStore} from "@nanostores/react";
-import {$isError} from "@src/store/store.ts";
 import styles from "./Error.module.css";
 
 
 const Error = () => {
-    const isError = useStore($isError);
-    if(!isError) return null;
 
     return (
         <Alert className={styles.error} variant="error">
