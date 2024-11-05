cc62949f95f611187796c6d6f17cbaeec1f061e7
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-11-05 11:03:37 +0100

    Skille mellom intern ingress mot API og url som brukes client-side

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -7,10 +7,16 @@ const TMS_MIN_SIDE_URL : EnvBasedUrl = {
     prod: "https://www.ansatt.nav.no/minside",
 };
 
-const TMS_VARSEL_API : EnvBasedUrl = {
+const API_INTERNAL_INGRESS: EnvBasedUrl = {
     local: "http://localhost:3000/tms-varsel-api",
     dev: "http://tms-varsel-api/tms-varsel-api",
     prod: "http://tms-varsel-api/tms-varsel-api",
+};
+
+const API_URL : EnvBasedUrl = {
+    local: "http://localhost:3000/tms-varsel-api",
+    dev: "https://www.ansatt.dev.nav.no/tms-varsel-api/tms-varsel-api",
+    prod: "https://www.nav.no/tms-varsel-api/tms-varsel-api",
 };
 
 
@@ -18,6 +24,6 @@ const TMS_VARSEL_API : EnvBasedUrl = {
 
 export const minSideUrl = TMS_MIN_SIDE_URL[environment];
 export const baseUrl = `${TMS_MIN_SIDE_URL[environment]}/varsler`;
-export const varslerApiurl = `${TMS_VARSEL_API[environment]}/alle`;
-export const inaktiverBeskjedApiUrl = `${TMS_VARSEL_API[environment]}/beskjed/inaktiver`;
+export const apiInternIngress = `${API_INTERNAL_INGRESS[environment]}/alle`;
+export const inaktiverBeskjedApiUrl = `${API_URL[environment]}/beskjed/inaktiver`;
 export const loginUrl = `/minside/varsler/oauth2/login?redirect=${baseUrl}`;
