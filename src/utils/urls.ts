4f77ffc918745786c1659afda9d625f85e7a8c52
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-11-06 15:12:04 +0100

    Egen 404 side

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,13 +1,19 @@
-const environment= import.meta.env.PUBLIC_APP_ENVIRONMENT;
+const environment = import.meta.env.PUBLIC_APP_ENVIRONMENT;
 type EnvBasedUrl = { [key in typeof environment]: string }
 
-const TMS_MIN_SIDE_URL : EnvBasedUrl = {
+const NAV_NO_URL = {
+    local: 'https://www.nav.no',
+    dev: 'https://www.ansatt.dev.nav.no',
+    prod: 'https://www.nav.no',
+};
+
+const MIN_SIDE_URL: EnvBasedUrl = {
     local: "http://localhost:4321/minside",
     dev: "https://www.ansatt.dev.nav.no/minside",
     prod: "https://www.nav.no/minside",
 };
 
-const APP_BASE_URL : EnvBasedUrl = {
+const APP_BASE_URL: EnvBasedUrl = {
     local: "http://localhost:4321",
     dev: "https://www.ansatt.dev.nav.no/minside/varsler",
     prod: "https://www.nav.no/minside/varsler",
@@ -20,19 +26,28 @@ const API_INTERNAL_INGRESS: EnvBasedUrl = {
     prod: "http://tms-varsel-api/tms-varsel-api",
 };
 
-const API_URL : EnvBasedUrl = {
+const API_URL: EnvBasedUrl = {
     local: "http://localhost:3000/tms-varsel-api",
     dev: "https://www.ansatt.dev.nav.no/tms-varsel-api",
     prod: "https://www.nav.no/tms-varsel-api",
 };
 
+const ERROR_REPORTING_URL = {
+    local:
+        'https://www.intern.dev.nav.no/person/kontakt-oss/nb/tilbakemeldinger/feil-og-mangler',
+    dev:
+        'https://www.ansatt.dev.nav.no/person/kontakt-oss/nb/tilbakemeldinger/feil-og-mangler',
+    prod:
+        'https://www.nav.no/person/kontakt-oss/nb/tilbakemeldinger/feil-og-mangler',
+};
 
 
-
-export const minSideUrl = TMS_MIN_SIDE_URL[environment];
+export const minSideUrl = MIN_SIDE_URL[environment];
 export const appBaseUrl = APP_BASE_URL[environment];
-export const baseUrl = `${TMS_MIN_SIDE_URL[environment]}/varsler`;
+export const baseUrl = `${MIN_SIDE_URL[environment]}/varsler`;
 export const apiInternIngress = `${API_INTERNAL_INGRESS[environment]}/alle`;
 export const inaktiverBeskjedApiUrl = `${API_URL[environment]}/beskjed/inaktiver`;
 export const loginUrl = `/minside/varsler/oauth2/login?redirect=${baseUrl}`;
 export const loginStepUpUrl = `${appBaseUrl}/oauth2/login?level=Level4&redirect_uri=${appBaseUrl}`;
+export const errorReportingUrl = ERROR_REPORTING_URL[environment];
+export const navNoUrl = NAV_NO_URL[environment];
