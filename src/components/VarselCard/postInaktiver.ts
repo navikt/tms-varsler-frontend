3b10ecce590de783788e6a8745fecf938da3e877
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-11-07 14:15:51 +0100

    Log istedenfor error

--- src/components/VarselCard/postInaktiver.ts
+++ src/components/VarselCard/postInaktiver.ts
@@ -14,9 +14,9 @@ const postInarkiver = (id: string) => {
     }).then(
         (response) => {
             if (!response.ok) {
-                console.error('Inaktivering av beskjed feilet med status: ' + response.status);
+                console.log('Inaktivering av beskjed feilet med status: ' + response.status);
             }
         }
-    ).catch((e) => console.error("Inaktivering av beskjed feilet"));
+    ).catch((e) => console.log("Inaktivering av beskjed feilet"));
 }
 export default postInarkiver;
