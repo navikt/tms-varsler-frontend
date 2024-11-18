30f81708b61100db7ba28c01957c1cc6fce749f9
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-11-18 09:36:49 +0100

    Formatting fix

--- src/components/VarselView/VarselView.tsx
+++ src/components/VarselView/VarselView.tsx
@@ -1,1 +1,41 @@
-import {useStore} from "@nanostores/react";import {$showTidligere, initVarsler, setError} from "@src/store/store.ts";import type {VarselResponse} from "@src/customTypes/Varsel.ts";import styles from "./VarselView.module.css"import {NyeVarslerView} from "./NyeVarslerView/NyeVarslerView.tsx";import {TidligereVarslerView} from "./TidligereVarslerView/TidligereVarslerView.tsx";import {Alert, Link} from "@navikt/ds-react";import {text} from "@language/text.ts";import {DOCUMENT_LOCALE} from "@language/language.ts";import Error from "@components/error/Error.tsx";import {loginStepUpUrl} from "@utils/urls.ts";import {logLinkNavigation} from "@utils/client/amplitude.ts";interface props {    varselResponse: VarselResponse,    isError: boolean}const VarselView = ({varselResponse, isError}: props) => {    const isTidligereView: boolean = useStore($showTidligere);    varselResponse && initVarsler(varselResponse);    return(    <div className={styles.varselViewContainer}>        <>            {isError && <Error />}            { varselResponse?.hasMaskedVarsel && <Alert variant="info"  >                {text.insufficientLoggingLevelAlert[DOCUMENT_LOCALE]}                <Link href={loginStepUpUrl} onClick={() => {                    logLinkNavigation("step-up-login")                }}>                    {text.insufficientLoggingLevelAlertLink[DOCUMENT_LOCALE]}                </Link>            </Alert>}            {isTidligereView ? <TidligereVarslerView/> :                <NyeVarslerView/>}        </>    </div>)}export default VarselView;
+import {useStore} from "@nanostores/react";
+import {$showTidligere, initVarsler} from "@src/store/store.ts";
+import type {VarselResponse} from "@src/customTypes/Varsel.ts";
+import styles from "./VarselView.module.css"
+import {NyeVarslerView} from "./NyeVarslerView/NyeVarslerView.tsx";
+import {TidligereVarslerView} from "./TidligereVarslerView/TidligereVarslerView.tsx";
+import {Alert, Link} from "@navikt/ds-react";
+import {text} from "@language/text.ts";
+import {DOCUMENT_LOCALE} from "@language/language.ts";
+import Error from "@components/error/Error.tsx";
+import {loginStepUpUrl} from "@utils/urls.ts";
+import {logLinkNavigation} from "@utils/client/amplitude.ts";
+
+interface props {
+    varselResponse: VarselResponse,
+    isError: boolean
+}
+
+const VarselView = ({varselResponse, isError}: props) => {
+    const isTidligereView: boolean = useStore($showTidligere);
+    varselResponse && initVarsler(varselResponse);
+
+    return(
+        <div className={styles.varselViewContainer}>
+            <>
+                {isError && <Error />}
+                { varselResponse?.hasMaskedVarsel && <Alert variant="info"  >
+                    {text.insufficientLoggingLevelAlert[DOCUMENT_LOCALE]}
+                    <Link href={loginStepUpUrl} onClick={() => {
+                        logLinkNavigation("step-up-login")
+                    }}>
+                        {text.insufficientLoggingLevelAlertLink[DOCUMENT_LOCALE]}
+                    </Link>
+                </Alert>}
+                {isTidligereView ? <TidligereVarslerView/> :
+                    <NyeVarslerView/>}
+            </>
+        </div>)
+}
+
+export default VarselView;
