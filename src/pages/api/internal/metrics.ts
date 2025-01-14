275e27e7ad6d0b28d84eab7298cfe29c92c98b19
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2025-01-14 16:48:27 +0100

    Prometheus

---
+++ src/pages/api/internal/metrics.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from "astro";
+import { register } from "prom-client";
+
+export const GET: APIRoute = async function get({ params, request }) {
+    return new Response(
+        await register.metrics(), {
+            status: 200,
+            headers: {
+                "Content-Type": register.contentType
+            }
+        });
+};
