8d6c82c68f815ed65536a0183a4808749477eac2
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-01-27 16:55:59 +0100

    Amplitude logge gjennom dekoratoren-moduler, for å ikke logge uten samtykke

--- src/utils/client/amplitude.ts
+++ src/utils/client/amplitude.ts
@@ -1,28 +1,25 @@
-import { init, track } from "@amplitude/analytics-browser";
+import {logAmplitudeEvent} from '@navikt/nav-dekoratoren-moduler';
 import type {VarselType} from "@src/customTypes/Varsel.ts";
+import {track} from "@amplitude/analytics-browser";
 
 
 const APP_NAME = "tms-varsler-frontend";
 
-export const logLinkNavigation = (komponent: string, destinasjon ?: string ) => {
-    track("navigere", { app: APP_NAME, komponent: komponent});
+export const logLinkNavigation = (komponent: string, destinasjon ?: string) => {
+    track("navigere", {app: APP_NAME, komponent: komponent});
+    logAmplitudeEvent({eventData: {komponent: komponent}, eventName: "click-tidligere-varsel-uten-link", origin: APP_NAME});
 }
 
 export const logClickInaktiverButton = () => {
-    track("click-inaktiver-button", { app: APP_NAME});
+    track("click-inaktiver-button", {app: APP_NAME});
+    logAmplitudeEvent({ eventName: "click-inaktiver-button", origin: APP_NAME});
+
 }
 
 export const logClickInaktivVarselWithoutLink = (type: VarselType) => {
-    track("click-tidligere-varsel-uten-link", { app: APP_NAME, varselType: type });
+
+    track("click-tidligere-varsel-uten-link", {app: APP_NAME, varselType: type});
 }
-
-
-export const initAmplitude = () => {
-    init("default", undefined, {
-        useBatch: true,
-        serverUrl: "https://amplitude.nav.no/collect-auto",
-        ingestionMetadata: {
-            sourceName: window.location.toString(),
-        },
-    });
-};
+export const logClickInaktivVarselWithoutLink2 = (type: VarselType) => {
+    logAmplitudeEvent({eventData: {varselType: type}, eventName: "click-tidligere-varsel-uten-link", origin: APP_NAME});
+}
