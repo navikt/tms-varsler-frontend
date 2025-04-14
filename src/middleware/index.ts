58bb33ddefd9a39f7b221e002465fe123af9154e
Nima Hakimi <74723240+nima-hakimi@users.noreply.github.com>
2025-04-14 13:51:52 +0200

    pino-logging

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,9 +2,11 @@ import { getToken, validateToken } from "@navikt/oasis";
 import { defineMiddleware } from "astro/middleware";
 import { loginUrl } from "@utils/urls.ts";
 const isLocal = import.meta.env.DEV;
+import pino from "pino-http";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const token = getToken(context.request.headers);
+  const logger = pino().logger;
   if (isLocal) {
     return next();
   }
@@ -14,13 +16,13 @@ export const onRequest = defineMiddleware(async (context, next) => {
   }
 
   if (!token) {
-    console.error("Token not found");
+    logger.error("Token not found");
     return context.redirect(loginUrl);
   }
 
   const validation = await validateToken(token);
   if (!validation.ok) {
-    console.error("Validation failed!");
+    logger.error("Validation failed!");
     return context.redirect(loginUrl);
   }
 
--- src/utils/server/getOboToken.ts
+++ src/utils/server/getOboToken.ts
@@ -1,17 +1,19 @@
 import { requestOboToken } from "@navikt/oasis";
+import pino from "pino-http";
 
 const isLocal = import.meta.env.PUBLIC_APP_ENVIRONMENT === "local";
 const audience = `${process.env.NAIS_CLUSTER_NAME}:min-side:tms-varsel-api`;
 
 export const getOboToken = async (token: string): Promise<string> => {
   const oboResult = await requestOboToken(token, audience);
+  const logger = pino().logger;
 
   if (isLocal) {
     return "Fake token";
   }
 
   if (!oboResult.ok) {
-    console.error("Error getting access token: " + oboResult.error);
+    logger.error("Error getting access token: " + oboResult.error);
     throw new Error("Request oboToken for tms-varsel-api failed ");
   }
 
