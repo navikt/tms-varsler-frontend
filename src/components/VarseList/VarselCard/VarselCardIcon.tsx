9b612e0e9af22b9e4c362ce7aa58351eaca273e2
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-06-24 20:24:12 +0200

    Formatting

--- src/components/VarseList/VarselCard/VarselCardIcon.tsx
+++ src/components/VarseList/VarselCard/VarselCardIcon.tsx
@@ -1,8 +1,9 @@
-import {ChatElipsisIcon, ClipboardIcon} from "@navikt/aksel-icons";
-import {VarselType} from "@src/customTypes/Varsel.ts";
+import { ChatElipsisIcon, ClipboardIcon } from "@navikt/aksel-icons";
+import { VarselType } from "@src/customTypes/Varsel.ts";
 
-
-export const VarselCardIcon = ({varselType}: { varselType: VarselType }) =>
-    varselType === VarselType.OPPGAVE ?
-        <ClipboardIcon aria-hidden={true} width="20px" height="20px"/> :
-        <ChatElipsisIcon aria-hidden={true} width="20px" height="20px"/>
+export const VarselCardIcon = ({ varselType }: { varselType: VarselType }) =>
+  varselType === VarselType.OPPGAVE ? (
+    <ClipboardIcon aria-hidden={true} width="20px" height="20px" />
+  ) : (
+    <ChatElipsisIcon aria-hidden={true} width="20px" height="20px" />
+  );
