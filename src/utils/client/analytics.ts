19773369321a6e8e17131684550e86318755cc96
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-09-19 10:39:03 +0200

    Formattering

--- src/utils/client/analytics.ts
+++ src/utils/client/analytics.ts
@@ -5,12 +5,19 @@ const APP_NAME = "tms-varsler-frontend";
 
 const analyticsLogger = getAnalyticsInstance("tms-microfrontend-template-ssr");
 
-export const logLinkNavigation = async (komponent: string, destinasjon?: string) => {
-  await analyticsLogger("navigere", { komponent: komponent, kategori: "tms-varsel", origin: APP_NAME});
+export const logLinkNavigation = async (
+  komponent: string,
+  destinasjon?: string,
+) => {
+  await analyticsLogger("navigere", {
+    komponent: komponent,
+    kategori: "tms-varsel",
+    origin: APP_NAME,
+  });
 };
 
 export const logClickInaktiverButton = async () => {
-  await analyticsLogger("click-inaktiver-button", { origin: APP_NAME});
+  await analyticsLogger("click-inaktiver-button", { origin: APP_NAME });
 };
 
 export const logClickInaktivVarselWithoutLink = async (type: VarselType) => {
